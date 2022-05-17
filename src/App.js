a3ec166512dac1e7c724819aea4b1533d13ab2ef
unknown <burulaiit>
2022-05-17 12:54:39 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import Change from './compomponents/Change'
+import { useState } from 'react';
 import './App.css';
 
+// const inputval={
+//   name:'Meri'
+
+// }
 function App() {
+  const[name,setName]=useState({})
+  // const [email, setEmail] = useState()
+	// const [phone, setPhone] = useState()
+  function nameChangeHandler(event){
+		setName((prevState)=>{
+      return{
+      ...prevState,
+val:event.target.value,
+
+    }
+    })
+	}
+//   function emailChangeHandler(event){
+// 		setValue((prevState)=>{
+//       return{
+//       ...prevState,
+// val:event.target.value,
+
+//     }
+//     })
+// 	}
+//   function phoneChangeHandler(event){
+// 		setValue((prevState)=>{
+//       return{
+//       ...prevState,
+// val:event.target.value,
+
+//     }
+//     })
+// 	}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {console.log(value)} */}
+      <input  onChange={nameChangeHandler}/>
+      {/* <input  onChange={emailChangeHandler}/>
+      <input  onChange={phoneChangeHandler}/> */}
+     <Change name={name.val}
+    //  email={email.val}
+    //  phone={email.val} 
+     />
     </div>
   );
 }
---
+++ src/compomponents/Change.js
@@ -0,0 +1,11 @@
+function Change(props){
+return (
+    <div>
+<h2>value:{props.name} </h2>
+{/* <h2>value:{props.email} </h2> <br/>
+ <h2>value:{props.phone} </h2> */}
+
+</div>
+)
+}
+export default Change
